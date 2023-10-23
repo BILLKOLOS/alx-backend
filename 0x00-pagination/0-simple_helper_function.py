ec14724c44b666264c2e692f88b7f1209d338565
billkolos <billooko2@gmail.com>
2023-10-23 04:49:53 -0700

    Write a function named index_range that takes two integer arguments page and page_size. > > The function should return a tuple of size two containing a start index and an end index corresponding to the range of indexes to return in a list for those particular pagination parameters. > > Page numbers are 1-indexed, i.e. the first page is page 1.

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
 """
-Simple helper function
+This module contains the function index_range that takes two integer arguments
+and returns a tuple containing a start index and an end index for a particular
+pagination page.
 """
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
         """
-            return a tuple of size two containing a start
-                 index and an end index corresponding to the
-                      range of indexes to return in a list for
-                           those particular pagination parameters
-                               :param page:
-                                   :param page_size:
-                                       :return:
-                                           """
-                                               start_index = (page - 1) * page_size
-                                                   end_index = start_index + page_size
-                                                       return start_index, end_index
+            Returns a tuple containing a start index and an end index for a particular
+                pagination page.
+
+                    Args:
+                            page (int): page number
+                                    page_size (int): number of items per page
+
+                                        Returns:
+                                                Tuple[int, int]: tuple containing a start index and an end index
+                                                    """
+                                                        if page <= 0 or page_size < 0:
+                                                                    return (-1, -1)
+
+                                                                    return (page_size * (page - 1), page_size * page)
